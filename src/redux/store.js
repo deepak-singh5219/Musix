de209e13d1a646beb32104586dd41ae9b09aad14
Deepak Singh <singhdeepak5219@gmail.com>
2023-04-03 14:43:29 +0530

    added spotifyApi to redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-
 import playerReducer from './features/playerSlice';
+import { spotifyApi } from './services/spotifyCore';
 
 export const store = configureStore({
   reducer: {
+    [spotifyApi.reducerPath]: spotifyApi.reducer,
     player: playerReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(spotifyApi.middleware),
 });
